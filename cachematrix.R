01d766456df16a2ab6ca840b5edc72f7a8605e64
Sergio Calderón Pérez-Lozao <sergiocalde94@gmail.com>
2017-12-16 12:25:46 +0100

    Added more comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,6 +1,9 @@
-## Second assignment of 'R Programming' course from Hopkins University, enrolled in Coursera
+## Second assignment of 'R Programming' course from Hopkins University, enrolled in Coursera.
+## This script includes two functions that will allow us to have an special matrix in our R environment
+## that will be useful when we have to deal with very large data. Since the native method matrix() has no
+## check if an identical matrix is alredy created in our workspace, this functionality will do.
 
-## This function allow us to create a 'special' cache matrix that we can use when we are dealing with very large data
+## This function allow us to create a 'special' cache matrix that we can use when we are dealing with very large data.
 
 makeCacheMatrix <- function(x = matrix()) {
   m <- NULL
@@ -17,7 +20,8 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## This function allow us to manage that 'special' cache matrix created with makeCacheMatrix
+## This function allow us to manage that 'special' cache matrix created with makeCacheMatrix. If the matrix is alredy
+## created, it won't be calculated again.
 
 cacheSolve <- function(x, ...) {
   m <- x$getinverse()
